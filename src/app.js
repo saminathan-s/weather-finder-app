9f5ab120114d6f797155e768ec32591a138ef824
Saminathan S <Saminathan_S@Singaporeair.com.sg>
2020-06-21 23:19:22 +0800

    updated prod configs

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 const publicPath = path.join(__dirname,'../public');
 const viewsPath = path.join(__dirname,'../templates/views');
@@ -81,6 +82,6 @@ app.get('*',(req,res)=>{
         name:'Sami'
     });
 })
-app.listen(3000,()=>{
+app.listen(port,()=>{
     console.log('Server is up and running!');
 });
