e8282b4fcd09e2b297d8f6bc80e31798d0d1af3f
Saminathan S <saminathan@Saminathans-MacBook-Pro.local>
2023-08-02 23:33:25 +0800

    Serverless changes

--- src/app.js
+++ src/app.js
@@ -1,87 +1,85 @@
-const path = require('path')
+const path = require('path');
 const express = require('express');
 const hbs = require('hbs');
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
 const app = express();
-const port = process.env.PORT || 3000;
 
-const publicPath = path.join(__dirname,'../public');
-const viewsPath = path.join(__dirname,'../templates/views');
-const partialsPath = path.join(__dirname,'../templates/partials');
+const publicPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
 
 app.use(express.static(publicPath));
 
 app.set('view engine', 'hbs');
-app.set('views',viewsPath);
+app.set('views', viewsPath);
 hbs.registerPartials(partialsPath);
 
-app.get('/',(req,res)=>{
-    res.render('index',{
-        title:'Weather App',
-        name:'Sami'
-    });
+app.get('/', (req, res) => {
+  res.render('index', {
+    title: 'Weather App',
+    name: 'Sami',
+  });
 });
 
-app.get('/about',(req,res)=>{
-    res.render('about',{
-        title:'About',
-        name:'Sami'
-    });
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About',
+    name: 'Sami',
+  });
 });
 
-app.get('/help',(req,res)=>{
-    res.render('help',{
-        title:'Help page',
-        name:'Sami'
-    });
+app.get('/help', (req, res) => {
+  res.render('help', {
+    title: 'Help page',
+    name: 'Sami',
+  });
 });
 
-app.get('/weather',(req,res)=>{
-    if(!req.query.address){
-        return res.send({
-            error:'You must provide an address'
-        });
+app.get('/weather', (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: 'You must provide an address',
+    });
+  }
+  let address = req.query.address;
+
+  geocode(address, (error, { latitude, longitude, location } = {}) => {
+    if (error) {
+      return res.send({
+        error,
+      });
     }
-    let address = req.query.address;
-
-    geocode(address,(error,{latitude, longitude, location} = {})=>{
-        if(error){
-            return res.send({
-                error
-            });
-        }
-        forecast(latitude,longitude,(error,forecast)=>{
-            if(error){
-                return res.send({
-                    error
-                })
-            }
-            res.send({
-                forecast,
-                location,
-                address
-            })
-        })
+    forecast(latitude, longitude, (error, forecast) => {
+      if (error) {
+        return res.send({
+          error,
+        });
+      }
+      res.send({
+        forecast,
+        location,
+        address,
+      });
     });
+  });
 });
 
-app.get('/help/*',(req,res)=>{
-    res.render('error',{
-        title:'Error Page',
-        data:'Help article not found. Please refine your search',
-        name:'Sami'
-    });
+app.get('/help/*', (req, res) => {
+  res.render('error', {
+    title: 'Error Page',
+    data: 'Help article not found. Please refine your search',
+    name: 'Sami',
+  });
 });
 
-app.get('*',(req,res)=>{
-    res.render('error',{
-        title:'Error Page',
-        data:'Page not found',
-        name:'Sami'
-    });
-})
-app.listen(port,()=>{
-    console.log('Server is up and running!');
+app.get('*', (req, res) => {
+  res.render('error', {
+    title: 'Error Page',
+    data: 'Page not found',
+    name: 'Sami',
+  });
 });
+
+module.exports = app;
